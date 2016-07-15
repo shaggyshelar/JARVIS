1c2841943bfa06e55003a2853c1130ac8a794886
SagarShelar <sagar.shelar@eternussolutions.com>
2016-07-15 11:25:51 +0530

    created addDevice page

--- webApp/src/app/components/addDevice.js
+++ webApp/src/app/components/addDevice.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
 
+const styles = {
+    addDevicePage: {
+        padding: 14
+    }
+}
 class AddDevice extends React.Component {
     render() {
-
         return (
-            <div>
-            AddDevice Page!!
+            <div style={styles.addDevicePage}>
+                 <Divider />
+                <Subheader>Device Info</Subheader>
+                <Divider />
+                <TextField
+                    floatingLabelText="Name"
+                    /><br />
+                <Divider />
+                <TextField
+                    floatingLabelText="Icon"
+                    /><br />
+                <Divider />
             </div>
         );
     }
