727c7fd79a223a20b484b4a437eef9bb2adbdaac
shaggyshelar <shaggy.shelar@gmail.com>
2016-07-15 19:32:37 +0530

    Updated AddLocation Component

--- webApp/src/app/components/addLocation.js
+++ webApp/src/app/components/addLocation.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import Toggle from 'material-ui/Toggle';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+    subHeader: {
+        backgroundColor: "whitesmoke",
+        fontSize: "16px",
+        fontWeight: "bold"
+    },
+    userPage: {
+        paddingLeft: 14,
+        paddingRight: 14
+    },
+    customMargin: {
+        marginTop: "15px",
+        marginBottom: "15px"
+    },
+}
 
 class AddLocation extends React.Component {
     render() {
 
         return (
             <div>
-            AddLocation Page!!
+                <TextField hintText="Name" style={styles.userPage} underlineShow={false} />
+                <Divider />
+                <TextField hintText="Icon" style={styles.userPage} underlineShow={false} />
+                <Divider />
+                <TextField hintText="Note" style={styles.userPage} underlineShow={false} />
+                <Divider />
+                <div style={styles.userPage}>
+                    <Toggle style={styles.customMargin}
+                        label="Featured"                      
+                        />
+                </div>
+                <Divider />
+                <div style={styles.userPage}>
+                    <div style={styles.customMargin}>
+                        <RaisedButton label="Add Location"  backgroundColor="#66CC33" />
+                    </div>
+                </div>
             </div>
         );
     }
