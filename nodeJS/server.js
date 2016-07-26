27caba428a6b15372c582172742feea26117cb5e
SagarShelar <sagar.shelar@eternussolutions.com>
2016-07-26 18:19:32 +0530

    Displaying device status changed

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,21 +1,23 @@
 var firebase = require("firebase");
 var accountFilePath = __dirname + '/SmartHome-a724a1bd5ec9.json';
-console.log(accountFilePath);
+var buttons = []
 
 firebase.initializeApp({
     serviceAccount: accountFilePath,
     databaseURL: "https://smarthome-46be4.firebaseio.com/"
 });
 
-console.log('Authenticated');
-
 var firebaseRef = firebase.database().ref('buttons');
-firebaseRef.on('value', function (snapshot) {
-    var items = [];
+firebaseRef.once('value', function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
         var item = childSnapshot.val();
         item['key'] = childSnapshot.key;
-        items.push(item);
+        buttons.push(item);
     });
-    console.log('Items',items);
+    console.log('Items', buttons);
+});
+
+firebaseRef.on('child_changed', function (data) {
+    var status = data.val().IsOn ? 'On' : 'Off';
+    console.log(data.key + ' is ' + status);
 });
